ce35997449619a64fd1f51a3979cac6c44c0b158
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-08-06 18:49:05 -0500

    Solucionando error en impresión de cards

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -9,12 +9,27 @@ let config = {
 firebase.initializeApp(config);
 let db = firebase.firestore();
 
+window.drawRanking = (ranking) => {
+  let response;
+  if (ranking === 'Aceptable') {
+    response = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+  }
+  if (ranking === 'Bueno') {
+    response = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>';
+  }
+  if (ranking === 'Excelente') {
+    response = '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>';
+  }
+  return response;
+};
+
 window.getRestaurantList = () => {
-  let result = '';
-  db.collection('places').orderBy('ranking', 'asc').get()
+  let response = '';
+  db.collection('places').orderBy('rate', 'desc').get()
     .then(result => {
       result.forEach(place => {
-        result += `<div class="col-md-4 my-4">
+        let ranking = drawRanking(place.data().rate);
+        response += `<div class="col-md-4 my-4">
                     <div class="frontside">
                         <div class="card">
                             <div class="card-body text-center">
@@ -24,11 +39,7 @@ window.getRestaurantList = () => {
                                 </p>
                                 <h4 class="card-title">${place.data().name}</h4>
                                 <p>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>  
+                                ${ranking}
                                 </p>
                                 <a href="#" class="btn btn-red btn-sm">
                                     <i class="fa fa-plus"></i>
@@ -37,7 +48,7 @@ window.getRestaurantList = () => {
                         </div>
                     </div>
             </div>`;
-        document.getElementById('restaurant').innerHTML = result;
+        document.getElementById('restaurant').innerHTML = response;
       });
     });
 };
